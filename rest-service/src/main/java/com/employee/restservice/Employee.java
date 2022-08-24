96a16e85873c1536dfb50bf443f5a592fd627c0e
yash.pandey <yash.pandey@mphasis.com>
2022-08-24 12:26:27 +0530

    Initialize Commit

---
+++ rest-service/src/main/java/com/employee/restservice/Employee.java
@@ -0,0 +1,46 @@
+package com.employee.restservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Employee {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String Fname;
+    private String Lname;
+    private String PhNo;
+
+    public String getFname() {
+        return Fname;
+    }
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+    public String getLname() {
+        return Lname;
+    }
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+    public String getPhNo() {
+        return PhNo;
+    }
+    public void setPhNo(String phNo) {
+        PhNo = phNo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
